daad111ad9eed824fec90dab4eee9be4fa11a017
Neil Hambleton <neil@ic.com.hk>
2011-08-15 11:32:40 +1200

    Temporarily remove email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-	validates :email, :presence => true, 
-                    :length => {:minimum => 3, :maximum => 254},
-                    :uniqueness => true,
-                    :email => true
+	#validates :email, :presence => true, 
+  #                  :length => {:minimum => 3, :maximum => 254},
+  #                  :uniqueness => true,
+  #                  :email => true
 
 end
